ed72cdf935adae5316a4c9f57b34d0fb7fc2fa6e
gracenho829 <gracenho829@gmail.com>
2021-11-15 17:28:06 +0900

    ai-trailer ui

--- lib/views/aiTrailer.dart
+++ lib/views/aiTrailer.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:trailer/controller/aiController.dart';
@@ -10,10 +12,75 @@ class AiTrailer extends GetView<AiController> {
   Widget build(BuildContext context) {
     return Scaffold(
       body:
-       Column(
-         children: [
-           Text("Hello"),
-         ],
+       Padding(
+         padding: const EdgeInsets.all(30.0),
+         child: 
+         Column(
+           crossAxisAlignment: CrossAxisAlignment.start,
+           children: [
+             Row(
+               children: [
+                Image(
+                    image: AssetImage('lib/images/ai-logo.png'),
+                    width: width * 0.4),
+                SizedBox(width: width*0.03),
+                Text(" 추천지", style:TextStyle(fontWeight: FontWeight.bold, color: Color(0xffFA8C78), fontSize: 25))
+              ],
+             ),
+            SizedBox(height: height*0.02),
+            Text("  사진을 올려서 여행지를 추천 받으세요!", style: TextStyle(fontSize: 15, color: Color(0xff494949), fontWeight: FontWeight.w500)),
+            SizedBox(height: height*0.04),
+           Container(
+             width: width,
+              child: 
+                Column(
+                  children: [
+                    IconButton(icon: Image.asset('lib/images/ai-add-icon.png'), iconSize: height*0.3, color: Color(0xff8B8B8B), onPressed:(){} ),
+                    ElevatedButton(onPressed: (){}, child: Ink(
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(colors: [Color(0xfffc96a0), Color(0xfffa9d90), Color(0xfff9a082),],),
+                            borderRadius: BorderRadius.circular(30.0)
+                        ),
+                        child: Container())
+                        )
+                  ],
+                ),
+              ),
+
+                  /*ElevatedButton(
+                      style: ButtonStyle(
+                        fixedSize: MaterialStateProperty.all(Size(width*0.7, height*0.05)),
+                        backgroundColor: MaterialStateProperty.all(Colors.transparent),
+                        foregroundColor: MaterialStateProperty.all(Colors.transparent),
+                        shadowColor: MaterialStateProperty.all(Colors.transparent),
+                      ),
+                      onPressed: () {},
+                      child: Ink(
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(colors: [Color(0xfffc96a0), Color(0xfffa9d90), Color(0xfff9a082),],),
+                            borderRadius: BorderRadius.circular(30.0)
+                        ),
+                        child: Container(
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.fromLTRB(5,0,15,0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.edit, color: Colors.white, size: 20),
+                              Text(
+                                "리뷰 작성하기",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.white
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),*/
+           ],
+         ),
        ),
     );
   }
